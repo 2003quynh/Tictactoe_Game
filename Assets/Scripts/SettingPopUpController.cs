239fc7d99b8d0114e47a9931924fc6dfb200504f
quynh2003 <ntquynh.work@gmail.com>
2024-05-15 09:31:59 +0700

    fix: update SettingPopUpController

--- Assets/Scripts/SettingPopUpController.cs
+++ Assets/Scripts/SettingPopUpController.cs
@@ -10,6 +10,10 @@ public class SettingPopUpController : MonoBehaviour
     [SerializeField] private float scaleDuration;
     [SerializeField] private GameObject backdrop;
     [SerializeField] private RectTransform settingPopup;
+    [SerializeField] private GameObject[] sounds, vibrations;
+
+    private bool _isOpenSound;
+    private bool _isOpenVibration;
    
     void Awake(){
         _isSettingShow = true;
@@ -31,5 +35,31 @@ public class SettingPopUpController : MonoBehaviour
         _isSettingShow = false;
         backdrop.SetActive(false);
         settingPopup.gameObject.SetActive(false);
+    }
+
+    public void ChangeSoundState(){
+        if (!_isOpenSound) {
+            sounds[0].SetActive(false);
+            sounds[1].SetActive(true);
+            _isOpenSound = true;
+        }
+        else {
+            sounds[1].SetActive(false);
+            sounds[0].SetActive(true);
+            _isOpenSound = false;
+        }
+    }
+
+    public void ChangeVibrationState(){
+        if (!_isOpenVibration) {
+            vibrations[0].SetActive(false);
+            vibrations[1].SetActive(true);
+            _isOpenVibration = true;
+        }
+        else {
+            vibrations[1].SetActive(false);
+            vibrations[0].SetActive(true);
+            _isOpenVibration = false;
+        }
     }
 }
